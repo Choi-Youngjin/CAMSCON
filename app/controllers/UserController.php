22f278f37e5af456615cedafe88abd904ba7e7b3
green <yyiemck@naver.com>
2015-02-10 16:39:47 +0900

    xnickname_first

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -4,22 +4,27 @@ use Facebook\FacebookJavaScriptLoginHelper;
 use Facebook\FacebookRequest;
 use Facebook\GraphUser;
 use Facebook\FacebookRequestException;
+
 class UserController extends BaseController {
+
 	public function showLoginRequired() {
 		if(!Session::has('intended')) {
 			Session::put('intended', url('/'));
 		}
 		return View::make('front.auth.login-required', ViewData::get());
 	}//showSignupPage()
+
 	public function signupUser() {
 		$response=new \stdClass();
 		$input=Input::only('email', 'nickname', 'password', 'password_confirmation', 'gender', 'remember');
+
 		$validationRules=array(
 			'email'=>array('required', 'email', 'unique:users,email'),
-			'nickname'=>array('required', 'min:2', 'unique:users,nickname'),
+			'nickname'=>array('required', 'min:2', 'unique:users,nickname', 'unique:restricted_nicknames,nickname'),
 			'password'=>array('required', 'min:8', 'confirmed'),
 			'gender'=>array('required', 'in:male,female')
 		);
+
 		$messages=array(
 			'required'=>':attribute_required',
 			'email'=>':attribute_email',
@@ -28,23 +33,30 @@ class UserController extends BaseController {
 			'confirmed'=>':attribute_confirmed',
 			'in'=>':attribute_in'
 		);
+
 		$validator=Validator::make($input, $validationRules, $messages);
+
 		if($validator->passes()) {
 			$user=new User;
 			$user->email=$input['email'];
 			$user->password=Hash::make($input['password']);
 			$user->nickname=$input['nickname'];
 			$user->gender=$input['gender'];
+			$user->enum="email";
+			$user->intro=null;
+
 			if($user->save()) {
 				$creds=array(
 					'email'=>$input['email'],
 					'password'=>$input['password'],
 				);
+
 				if($input['remember']) {
 					$remember=true;
 				} else {
 					$remember=false;
 				}
+
 				if(Auth::attempt($creds, $remember)) {
 					$response->type='success';
 					$response->msg=$this->userBoxTemplate();
@@ -61,19 +73,26 @@ class UserController extends BaseController {
 			$response->type='error';
 			$response->msg=$validator->messages()->first();
 		}
+
 		return Response::json($response);
 	}//signupUser()
+
 	public function loginWithFB() {
 		if(Auth::check()) {
 			Auth::logout();
 		}
+
 		//Setup response obj
 		$response=new stdClass();
+
 		//Init FB SDK
 		FacebookSession::setDefaultApplication(Config::get('app.fb_app_id'), Config::get('app.fb_app_secret'));
+
 		//Validate login
 		$loginHelper = new FacebookJavaScriptLoginHelper();
+
 		$session=null;
+
 		try {
 			$session=$loginHelper->getSession();
 		} catch(FacebookRequestException $ex) {
@@ -85,6 +104,7 @@ class UserController extends BaseController {
 			$response->type='error';
 			$response->msg='fb_validation_error';
 		}
+
 		if($session) {// Logged in
 			//Get fb user id
 			$fbUserId=$session->getSessionInfo()->asArray()['user_id'];
@@ -95,6 +115,7 @@ class UserController extends BaseController {
 			if($user) {
 				//User account associated with the fb user id exists. Login user.
 				Auth::login($user);
+
 				$response->type='success';
 				$response->msg=$this->userBoxTemplate();
 				$response->json=$user->toJson();
@@ -105,6 +126,7 @@ class UserController extends BaseController {
 					$user_profile = (new FacebookRequest(
 						$session, 'GET', '/me'
 					))->execute()->getGraphObject(GraphUser::className());
+
 					$fbPicture = (new FacebookRequest(
 						$session, 'GET', '/me/picture', 
 						array(
@@ -114,10 +136,13 @@ class UserController extends BaseController {
 							'height'=>200
 						)
 					))->execute()->getGraphObject();
+
 					$user=new User;
-					$user->email=$user_profile->getProperty('email');
+					$user->email=null;
+					$user->enum="facebook";
 					$user->password=null;
-					$user->nickname=$user_profile->getName();
+					$user->nickname=$fbNickname;
+					$user->intro=null;
 					$gender=$user_profile->getProperty('gender');
 					if($gender=='male') {
 						$user->gender='male';
@@ -126,6 +151,7 @@ class UserController extends BaseController {
 					} else {
 						$user->gender=null;
 					}
+
 					if($user->save()) {
 						$fbAccount=new FacebookAccount;
 						$fbAccount->user_id=$user->id;
@@ -137,6 +163,7 @@ class UserController extends BaseController {
 						$fbAccount->link=$user_profile->getLink();
 						$fbAccount->gender=$user_profile->getProperty('gender');
 						$fbAccount->locale=$user_profile->getProperty('locale');
+
 						if($fbPicture->getProperty('is_silhouette')===false) {
 							$profileImage=new ProfileImage;
 							try {
@@ -152,10 +179,13 @@ class UserController extends BaseController {
 						try {
 							$fbAccount->save();
 							if(is_object($profileImage)) {
-								$profileImage->save();
+								$profileImageImage->save();
 							}
+
 							DB::commit();
+
 							Auth::login($user);
+
 							$response->type='success';
 							$response->msg=$this->userBoxTemplate();
 							$response->json=$user->toJson();
@@ -175,23 +205,30 @@ class UserController extends BaseController {
 				}
 			}
 		}
+
 		return Response::json($response);
 	}//loginWithFB()
+
 	public function loginWithEmail() {
 		if(Auth::check()) {
 			Auth::logout();
 		}
+
 		$input=Input::only('email','password','remember');
+
 		$creds=array(
 			'email'=>$input['email'],
 			'password'=>$input['password'],
 		);
+
 		if($input['remember']) {
 			$remember=true;
 		} else {
 			$remember=false;
 		}
+
 		$response=new \stdClass();
+
 		if (Auth::attempt($creds, $remember)) {
 			$response->type='success';
 			$response->msg=$this->userBoxTemplate();
@@ -199,14 +236,18 @@ class UserController extends BaseController {
 		} else {
 			$response->type='error';
 		}
+
 		return Response::json($response);
 	}//loginWithEmail()
+
 	public function logoutUser() {
 		Auth::logout();
 		return Redirect::back();
 	}//logoutUser()
+
 	private function userBoxTemplate() {
 		$userBox=View::make('includes.user-box')->render();
 		return $userBox;
 	}//userBoxTemplate()
+
 }
---
+++ app/models/RestrictedNicknames.php
@@ -0,0 +1,6 @@
+<?php
+
+class RestrictedNicknames extends Eloquent {
+
+	protected $table = 'restricted_nicknames';
+}
