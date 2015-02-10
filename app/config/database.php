2a9bd1c9af2c77dfc35b89b5091dc534a77bf3cd
green <yyiemck@naver.com>
2015-02-10 17:18:48 +0900

    db mod2

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'lara',
+			'database'  => '',
 			'username'  => 'root',
-			'password'  => 'rhd1901',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
